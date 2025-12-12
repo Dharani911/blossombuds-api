dbf2a8c3e3779bb06a2c9e88e5c8f43c3eac8e94
Dharani911 <dharanip911@gmail.com>
2025-12-12 23:09:22 +0100

    redix fix 3- 12/12/2025

--- blossombuds-api/src/main/java/com/blossombuds/domain/Product.java
+++ blossombuds-api/src/main/java/com/blossombuds/domain/Product.java
@@ -1,6 +1,7 @@
 package com.blossombuds.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
@@ -97,3 +98,4 @@ public class Product {
         }
         return null;
     }
+}
