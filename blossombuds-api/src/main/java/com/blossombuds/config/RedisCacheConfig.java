7a92738beafebc45c7789b33fc635cd89ea59463
Dharani911 <dharanip911@gmail.com>
2025-12-13 23:43:51 +0100

    redis cache config is set- 13/12/2025

---
+++ blossombuds-api/src/main/java/com/blossombuds/config/RedisCacheConfig.java
@@ -0,0 +1,33 @@
+package com.blossombuds.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory cf) {
+
+        // Redis-only mapper (DO NOT make this a global @Bean ObjectMapper)
+        ObjectMapper redisOm = new ObjectMapper().findAndRegisterModules();
+
+        GenericJackson2JsonRedisSerializer valueSerializer =
+                new GenericJackson2JsonRedisSerializer(redisOm);
+
+        RedisCacheConfiguration cfg = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer));
+
+        return RedisCacheManager.builder(cf).cacheDefaults(cfg).build();
+    }
+}
